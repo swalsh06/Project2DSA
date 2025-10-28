d576fa1777a55d471453e808c87d5c06fdb4d446
diya <pateld6@ufl.edu>
2025-10-28 16:53:00 -0400

    Display Screen

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 #include "star.h"
+
+#include "display.h"
+
+int main() {
+    Display display;
+
+    // Temporary simulated Dijkstra result
+    std::vector<int> testPath = {2, 45, 77, 120, 300};
+    display.setPath(testPath);
+
+    display.start();
+    return 0;
+}
+
