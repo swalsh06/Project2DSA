c065c494041e21c2037903e627cfca421142923f
Charley Kennedy <charleykennedy@ufl.edu>
2025-11-02 15:00:31 -0500

    Algorithm functions incorporated into StarGroup class

--- StarGroup.h
+++ StarGroup.h
@@ -8,19 +8,25 @@
 #include <cmath>
 #include <string>
 #include <map>
+#include <unordered_map>
 #include "star.h"
 using namespace std;
 
 class StarGroup {
     vector<Star> stars;
     map<int, vector<pair<int, float>>> adjacencyList;
+    int nodesExplored;
   public:
     StarGroup(vector<Star> s);
     map<int, vector<pair<int, float>>> getAdjacencyList();
     void printAdjacencyList();
     void formAdjacencyList();
     double calcDistance(int i, int j);
+    vector<int> dijkstra(const unordered_map<int, vector<pair<int,float>>>& graph, int start, int target);
+    float heuristic(int from, int to, const unordered_map<int, pair<float,float>>& starPos);
+    vector<int> a_star(const unordered_map<int, vector<pair<int,float>>>& graph, const unordered_map<int, pair<float,float>>& starPos, int start, int end);
 };
+
 
 
 #endif //STARGROUP_H
--- dijkstra.h
+++
@@ -1,4 +0,0 @@
-#ifndef DIJKSTRA_H
-#define DIJKSTRA_H
-
-#endif //DIJKSTRA_H
